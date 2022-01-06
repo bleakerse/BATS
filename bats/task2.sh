bef836cc5a474f11258f52d0d8a75d839f202eb1
bleakerse <97189131+bleakerse@users.noreply.github.com>
2022-01-06 13:13:39 +0000

    Update task2.sh

--- bats/task2.sh
+++ bats/task2.sh
@@ -42,13 +42,13 @@ totalRAM=$((totalRAM / 1024))
 echo "The host RAM size is $totalRAM GB"
  
 # Compare user inputted required RAM to actual Host RAM size
-if [ $totalRAM -eq $1 ]
+if [ $totalRAM -gt $1 ]
 then
     echo "SUCCESS. Host has equal memory required"
 elif [ $totalRAM -lt $1 ]
 then
     echo "ERROR. Host has less memory than required"
-elif [ $totalRAM -lt $1 ]
+elif [ $totalRAM -gt $1 ]
 then
     echo "WARNING. Host has more memory than required"
 fi
